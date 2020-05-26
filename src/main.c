855ab7a078d081d9ae2d1da9cc3a2c045123285a
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-26 16:43:38 -0400

    se agrega getopt y el recibimiento de parametros

--- src/main.c
+++ src/main.c
@@ -1,8 +1,60 @@
+#include <ctype.h>
 #include <stdio.h>
-#include "../incl/example.h" 
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[])
+int
+main (int argc, char **argv)
 {
-	printExample("This is a example of a complete program in C :D ");
-	return 0; 
+  int cantImagenes = 0;
+  int umbralBin = 0;
+  int umbralNeg = 0;
+  int flagConclusion = 0;
+  char *nombreArchivoMasc = NULL;
+  int index;
+  int c;
+
+  opterr = 0;
+
+
+  while ((c = getopt (argc, argv, "c:u:m:n:b")) != -1)
+    switch (c)
+		{
+		case 'c':
+			sscanf(optarg, "%d", &cantImagenes);
+			break;
+		case 'u':
+			sscanf(optarg, "%d", &umbralBin);
+			break;
+		case 'n':
+			sscanf(optarg, "%d", &umbralNeg);
+			break;
+		case 'm':
+			nombreArchivoMasc = optarg;
+			break;
+		case 'b':
+			flagConclusion = 1;
+			break;
+		case '?':
+			if (optopt == 'c')
+			fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+			else if (isprint (optopt))
+			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+			else
+			fprintf (stderr,
+					"Unknown option character `\\x%x'.\n",
+					optopt);
+			return 1;
+		default:
+			abort ();
+		}
+
+
+  printf ("cantidad Imagenes = %d\n, umbral Binario = %d\n, umbral Negrura = %d\n, Nombre archivo Mascara = %s\n, flag Conclusion = %d\n",
+          cantImagenes, umbralBin, umbralNeg, nombreArchivoMasc, flagConclusion);
+
+  for (index = optind; index < argc; index++)
+    printf ("Non-option argument %s\n", argv[index]);
+  return 0;
 }
+
