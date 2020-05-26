30ca7dd80730481842a9faa34dc06993eaaa60ea
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-26 16:53:57 -0400

    creacion de archivos para la lectura de imagenes

---
+++ src/lecturaImagenes.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../incl/lecturaImagenes.h"
+
+/*
+ * Aqui se escriben las funciones para leer imagenes 
+*/
--- src/main.c
+++ src/main.c
@@ -3,58 +3,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int
-main (int argc, char **argv)
+//Funcion Main
+int main (int argc, char **argv)
 {
-  int cantImagenes = 0;
-  int umbralBin = 0;
-  int umbralNeg = 0;
-  int flagConclusion = 0;
-  char *nombreArchivoMasc = NULL;
-  int index;
-  int c;
+	//Inicialización de Variables
+	int cantImagenes = 0;
+	int umbralBin = 0;
+	int umbralNeg = 0;
+	int flagConclusion = 0;
+	char *nombreArchivoMasc = NULL;
+	int index;
+	int c;
 
-  opterr = 0;
+	opterr = 0;
+
+	//el siguiente ciclo se utiliza para recibir los parametros de entrada usando getopt
+	while ((c = getopt (argc, argv, "c:u:m:n:b")) != -1)
+		switch (c)
+			{
+			case 'c':
+				sscanf(optarg, "%d", &cantImagenes);
+				break;
+			case 'u':
+				sscanf(optarg, "%d", &umbralBin);
+				break;
+			case 'n':
+				sscanf(optarg, "%d", &umbralNeg);
+				break;
+			case 'm':
+				nombreArchivoMasc = optarg;
+				break;
+			case 'b':
+				flagConclusion = 1;
+				break;
+			case '?':
+				if (optopt == 'c')
+				fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+				fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+				fprintf (stderr,
+						"Unknown option character `\\x%x'.\n",
+						optopt);
+				return 1;
+			default:
+				abort ();
+			}
 
 
-  while ((c = getopt (argc, argv, "c:u:m:n:b")) != -1)
-    switch (c)
-		{
-		case 'c':
-			sscanf(optarg, "%d", &cantImagenes);
-			break;
-		case 'u':
-			sscanf(optarg, "%d", &umbralBin);
-			break;
-		case 'n':
-			sscanf(optarg, "%d", &umbralNeg);
-			break;
-		case 'm':
-			nombreArchivoMasc = optarg;
-			break;
-		case 'b':
-			flagConclusion = 1;
-			break;
-		case '?':
-			if (optopt == 'c')
-			fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-			else if (isprint (optopt))
-			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-			else
-			fprintf (stderr,
-					"Unknown option character `\\x%x'.\n",
-					optopt);
-			return 1;
-		default:
-			abort ();
-		}
+	printf ("cantidad Imagenes = %d\n, umbral Binario = %d\n, umbral Negrura = %d\n, Nombre archivo Mascara = %s\n, flag Conclusion = %d\n",
+			cantImagenes, umbralBin, umbralNeg, nombreArchivoMasc, flagConclusion);
 
-
-  printf ("cantidad Imagenes = %d\n, umbral Binario = %d\n, umbral Negrura = %d\n, Nombre archivo Mascara = %s\n, flag Conclusion = %d\n",
-          cantImagenes, umbralBin, umbralNeg, nombreArchivoMasc, flagConclusion);
-
-  for (index = optind; index < argc; index++)
-    printf ("Non-option argument %s\n", argv[index]);
-  return 0;
-}
+	for (index = optind; index < argc; index++)
+		printf ("Non-option argument %s\n", argv[index]);
+	return 0;
+	}
 
