013a56b68ce753a1e67005bd5fd6510e2895d382
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-27 16:50:44 -0400

    creacion de conversion a escala de grises

---
+++ incl/conversion.h
@@ -0,0 +1,10 @@
+#ifndef CONVERSION_H
+#define CONVERSION_H
+/*
+ * Aqui se colocan las cabeceras de las funciones
+ * que se creen en el archivo conversion.c
+ */
+
+JpegData convertirAEscalaGrises(JpegData img);
+
+#endif
