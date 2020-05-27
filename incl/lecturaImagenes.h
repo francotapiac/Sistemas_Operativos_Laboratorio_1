e449e815c1dace4a55573b7e0c8ad569b6a78f26
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-27 17:14:28 -0400

    preparacion para el merge

--- incl/lecturaImagenes.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXAMPLE_H
-#define EXAMPLE_H
-/*
- * Aqui se colocan las cabeceras de las funciones
- * que se creen en el archivo lecturaImagenes.c
- */
-
-#endif
--- src/lecturaImagenes.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "../incl/lecturaImagenes.h"
-
-/*
- * Aqui se escriben las funciones para leer imagenes 
-*/
