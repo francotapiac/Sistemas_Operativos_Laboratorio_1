dc4acd412dc422f0282d6d203aba714ca59b6037
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-28 00:28:54 -0400

    se crea la binarizacion

---
+++ src/binarizacion.c
@@ -0,0 +1,20 @@
+#include "../incl/binarizacion.h"
+#include <stdint.h>
+#include <stdio.h>
+
+JpegData binarizarImagen(JpegData img, int umbral){
+
+    int len = img.width*img.height;
+
+    for (int i = 0; i < len; i++)
+    {
+        if(img.data[i] > umbral){
+            img.data[i] = 255;
+        }
+        else{
+            img.data[i] = 0;
+        }
+    }
+
+    return img; 
+}
